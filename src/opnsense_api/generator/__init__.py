712973412728bd1e7c7482ac2943a1265563ceca
Eric Davis <6662995+endavis@users.noreply.github.com>
2025-11-25 08:48:24 +0000

    chore: remove legacy CodeGenerator in favor of OpenAPI workflow

--- src/opnsense_api/generator/__init__.py
+++ src/opnsense_api/generator/__init__.py
@@ -1,6 +1,5 @@
 """Python code generator for OPNsense API client classes."""
 
-from .code_generator import CodeGenerator
 from .openapi_generator import OpenApiGenerator
 
-__all__ = ["CodeGenerator", "OpenApiGenerator"]
+__all__ = ["OpenApiGenerator"]
